712d80a577dee2ce73569ffac76cf280f723659f
Julie-Des <julie.deshayes14@gmail.com>
2025-10-23 19:16:34 +0200

    fix: config.js file

--- FrontEnd/assets/config.js
+++ FrontEnd/assets/config.js
@@ -1,3 +1,1 @@
-const API_URL = window.location.hostname.includes("localhost")
-  ? "http://localhost:5678"
-  : "https://projet6-oc.onrender.com";
+const API_URL = "https://projet6-oc.onrender.com";
